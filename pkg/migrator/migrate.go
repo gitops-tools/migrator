752154d2744f7d6c4465e5510c632eea9992a337
Kevin McDermott <bigkevmcd@gmail.com>
2024-10-02 09:49:16 +0100

    Use a list to query resources when no name is provided.
    This is the initial fix for the all resources case.

--- pkg/migrator/migrate.go
+++ pkg/migrator/migrate.go
@@ -9,6 +9,8 @@ import (
 )
 
 // MigrateUp executes the migrations forward.
+// TODO: option for batching!
+// TODO: option for dry-run (do not do the patch).
 func MigrateUp(ctx context.Context, kubeClient client.Client, migrations []Migration) error {
 	return migrate(ctx, kubeClient, migrations, func(m Migration) []Patch {
 		return m.Up
@@ -24,23 +26,55 @@ func MigrateDown(ctx context.Context, kubeClient client.Client, migrations []Mig
 
 func migrate(ctx context.Context, kubeClient client.Client, migrations []Migration, f func(Migration) []Patch) error {
 	for _, migration := range migrations {
-		u := &unstructured.Unstructured{}
-		u.SetGroupVersionKind(migration.TargetGroupVersionKind())
-
-		if err := kubeClient.Get(ctx, migration.TargetObjectKey(), u); err != nil {
-			return fmt.Errorf("getting migration target %s %s: %w", u.GetKind(), migration.TargetObjectKey(), err)
-		}
-
-		updated, err := ApplyPatches(u, f(migration))
+		toMigrate, err := resourcesToMigrate(ctx, kubeClient, migration)
 		if err != nil {
 			return err
 		}
 
-		if err := kubeClient.Patch(ctx, updated, client.MergeFrom(u)); err != nil {
-			// TODO
-			return err
+		for _, resource := range toMigrate {
+			updated, err := ApplyPatches(&resource, f(migration))
+			if err != nil {
+				return err
+			}
+
+			if err := kubeClient.Patch(ctx, updated, client.MergeFrom(&resource)); err != nil {
+				// TODO
+				return err
+			}
 		}
 	}
 
 	return nil
+}
+
+func resourcesToMigrate(ctx context.Context, kubeClient client.Reader, migration Migration) ([]unstructured.Unstructured, error) {
+	target := migration.TargetObjectKey()
+
+	if target.Name != "" {
+		return singleResource(ctx, kubeClient, target, migration)
+	}
+
+	return multiResources(ctx, kubeClient, target, migration)
+}
+
+func singleResource(ctx context.Context, kubeClient client.Reader, target client.ObjectKey, migration Migration) ([]unstructured.Unstructured, error) {
+	u := unstructured.Unstructured{}
+	u.SetGroupVersionKind(migration.TargetGroupVersionKind())
+
+	if err := kubeClient.Get(ctx, target, &u); err != nil {
+		return nil, fmt.Errorf("getting migration target %s %s: %w", u.GetKind(), target, err)
+	}
+
+	return []unstructured.Unstructured{u}, nil
+}
+
+func multiResources(ctx context.Context, kubeClient client.Reader, target client.ObjectKey, migration Migration) ([]unstructured.Unstructured, error) {
+	ul := unstructured.UnstructuredList{}
+	ul.SetGroupVersionKind(migration.TargetGroupVersionKind())
+
+	if err := kubeClient.List(ctx, &ul); err != nil {
+		return nil, fmt.Errorf("getting migration targets %s %s: %w", ul.GetKind(), target, err)
+	}
+
+	return ul.Items, nil
 }
--- pkg/migrator/migrate_test.go
+++ pkg/migrator/migrate_test.go
@@ -180,8 +180,74 @@ func TestMigrateDown(t *testing.T) {
 	}
 }
 
-func createService() *corev1.Service {
-	return &corev1.Service{
+func TestMigrateUp_multiple_resources(t *testing.T) {
+	migrations := []Migration{
+		{
+			Name:     "patch-service",
+			Filename: "testdata/simple.yaml",
+			Target: types.PatchTarget{
+				Gvk: gvk.Gvk{
+					Group:   "",
+					Version: "v1",
+					Kind:    "Service",
+				},
+				Namespace: "default",
+			},
+			Up: []Patch{
+				{
+					Type:   "application/json-patch+json",
+					Change: `[{"op":"replace","path":"/spec/ports/0/port","value":81}]`,
+				},
+			},
+		},
+	}
+
+	fc := fake.NewClientBuilder().WithObjects(
+		createService(withName("svc-1")),
+		createService(withName("svc-2"))).Build()
+
+	if err := MigrateUp(context.TODO(), fc, migrations); err != nil {
+		t.Fatal(err)
+	}
+
+	var svcList corev1.ServiceList
+	if err := fc.List(context.TODO(), &svcList, client.InNamespace("default")); err != nil {
+		t.Fatal(err)
+	}
+
+	want := [][]corev1.ServicePort{
+		{
+			{Protocol: "TCP",
+				Port:       81,
+				TargetPort: intstr.FromInt(9376),
+			},
+		},
+		{
+			{
+				Protocol:   "TCP",
+				Port:       81,
+				TargetPort: intstr.FromInt(9376),
+			},
+		},
+	}
+
+	ports := collect(svcList.Items, func(s corev1.Service) []corev1.ServicePort {
+		return s.Spec.Ports
+	})
+
+	if diff := cmp.Diff(want, ports); diff != "" {
+		t.Errorf("failed to migrate:\n%s", diff)
+	}
+}
+
+func withName(s string) func(*corev1.Service) {
+	return func(svc *corev1.Service) {
+		svc.SetName(s)
+	}
+}
+
+func createService(opts ...func(*corev1.Service)) *corev1.Service {
+	svc := &corev1.Service{
 		TypeMeta: metav1.TypeMeta{
 			Kind:       "Service",
 			APIVersion: "v1",
@@ -203,4 +269,20 @@ func createService() *corev1.Service {
 			},
 		},
 	}
+
+	for _, opt := range opts {
+		opt(svc)
+	}
+
+	return svc
+}
+
+func collect[T any, R any](input []T, pred func(T) R) []R {
+	result := []R{}
+
+	for _, v := range input {
+		result = append(result, pred(v))
+	}
+
+	return result
 }
